2b99a5cdf8dd8d8592e4c8db7370f61960b9afad
DanEliyahu <dan4uni@gmail.com>
2024-07-09 20:07:59 +0300

    Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,36 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private int speed;
+
     private PlayerControls _playerControls;
+    private Rigidbody _rb;
+    private Vector3 _movement;
 
     private void Awake()
     {
         _playerControls = new PlayerControls();
+        _rb = GetComponent<Rigidbody>();
+        _movement = new Vector3();
     }
 
     private void OnEnable()
     {
         _playerControls.Enable();
     }
-    
+
     private void Update()
     {
-        var movement = _playerControls.Player.Move.ReadValue<Vector2>();
-        print(movement);
+        var input = _playerControls.Player.Move.ReadValue<Vector2>();
+        _movement.x = input.x;
+        _movement.z = input.y;
+        _movement.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.MovePosition(transform.position + (_movement * (speed * Time.fixedDeltaTime)));
     }
 }
