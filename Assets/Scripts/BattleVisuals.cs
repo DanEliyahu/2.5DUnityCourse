9feb07d5005ee11e0b2af343a23a9b7e8bc8cc3a
DanEliyahu <dan4uni@gmail.com>
2024-08-10 16:04:22 +0300

    Integrating Battle Animations

--- Assets/Scripts/BattleVisuals.cs
+++ Assets/Scripts/BattleVisuals.cs
@@ -7,8 +7,16 @@ public class BattleVisuals : MonoBehaviour
     [SerializeField] private Slider healthBar;
     [SerializeField] private TextMeshProUGUI levelText;
 
+    private Animator _animator;
+
+    private static readonly int Attack = Animator.StringToHash("Attack");
+    private static readonly int Hit = Animator.StringToHash("Hit");
+    private static readonly int Die = Animator.StringToHash("Die");
+
+
     private void Start()
     {
+        _animator = GetComponent<Animator>();
         SetStartingValues(10, 15, 56);
     }
 
@@ -17,5 +25,30 @@ public class BattleVisuals : MonoBehaviour
         healthBar.maxValue = maxHealth;
         healthBar.value = currentHealth;
         levelText.text = level.ToString();
+    }
+
+    public void ChangeHealth(int newValue)
+    {
+        healthBar.value = newValue;
+        if (newValue <= 0)
+        {
+            PlayDeathAnimation();
+            Destroy(gameObject, 1f);
+        }
+    }
+
+    public void PlayAttackAnimation()
+    {
+        _animator.SetTrigger(Attack);
+    }
+
+    public void PlayHitAnimation()
+    {
+        _animator.SetTrigger(Hit);
+    }
+
+    public void PlayDeathAnimation()
+    {
+        _animator.SetTrigger(Die);
     }
 }
